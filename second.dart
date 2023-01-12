892268da5283ba1fb754be5962a0de476d9f936b
Mark Stephen Thomas <markstephenthomas10@gmail.com>
2023-01-12 10:43:36 +0530

    High order functions

--- second.dart
+++ second.dart
@@ -1,1 +1,23 @@
+main(){
+    printChefsName(getChefFromDb, 4);
+}
+void printChefsName(String Function(int id) getChefFromDb,int id){
+    String name=getChefFromDb(id);
+    print(name);
+}
+String getChefFromDb(int id){
+    switch(id){
+        case 1:
+            return "Mark";
+            break;
+        case 2:
+            return "Hans";
+            break;
+        case 3:
+            return "Thomas";
+            break;
+        default:
+            return "No name found";
 
+    }
+}
